f6f7dfd615bc92a84fd7caec253fee6e22a688ef
Diva1024 <80517970+Diva1024@users.noreply.github.com>
2022-10-14 18:40:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video="";
+status1=true;
+function preload(){
+video=createVideo("video.mp4");
+video.hide();
+}
+function setup(){
+canvas=createCanvas(450,300);
+canvas.center();
+}
+function draw(){
+image(video,0,0,450,300);
+}
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting objects";
+    
+}
+function modelLoaded(){
+    console.log("modelloaded");
+    status1=true;
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+}
